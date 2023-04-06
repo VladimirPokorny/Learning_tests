69a1b907cc77e61b3cd11385e20df297c82e1bec
vladimir_pokorny <vladimir.pokorny@tescan.com>
2023-04-06 13:39:54 +0200

    pycodestyle updates

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import numpy as np
 
+
 def pythagoras(a: float, b: float) -> float:
     c = np.sqrt(a**2 + b**2)
     return c
+
+
+def test():
+    return
--- test.py
+++ test.py
@@ -1,16 +1,15 @@
-# import numpy as np
 from main import pythagoras
 
 
 def test_pythagoras():
-        a = 3
-        b = 4
-        expected_result = 5        
-        
-        result = pythagoras(a, b)
+    a = 3
+    b = 4
+    expected_result = 5
 
-        assert result == expected_result
-    
+    result = pythagoras(a, b)
+
+    assert result == expected_result
+
 
 if __name__ == "__main__":
     test_pythagoras()
