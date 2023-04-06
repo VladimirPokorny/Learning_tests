b683de177556761ba9e6e560d4e8f57c722dc606
vladimir_pokorny <vladimir.pokorny@tescan.com>
2023-04-06 13:27:42 +0200

    updated test func

--- test.py
+++ test.py
@@ -5,8 +5,13 @@ from main import pythagoras
 def test_pythagoras():
         a = 3
         b = 4
-        expected_result = 5
+        expected_result = 5        
         
         result = pythagoras(a, b)
-        
-        self.assertEqual(result, expected_result)
+
+        assert result == expected_result
+    
+
+if __name__ == "__main__":
+    test_pythagoras()
+    print("Everything passed")
