166bb5ea0d9b55749e53c60e8f4cedef2f78faf6
vladimir_pokorny <vladimir.pokorny@tescan.com>
2023-04-06 13:23:27 +0200

    eddited test func

--- test.py
+++ test.py
@@ -3,17 +3,10 @@ from main import pythagoras
 
 
 def test_pythagoras():
-    # Test a few cases
-    assert np.isclose(pythagoras(3, 4), 5)
-    assert np.isclose(pythagoras(5, 12), 13)
-    assert np.isclose(pythagoras(7, 24), 25)
-
-    # Test some edge cases
-    assert np.isclose(pythagoras(0, 0), 0)
-    assert np.isclose(pythagoras(1, 0), 1)
-    assert np.isclose(pythagoras(0, 1), 1)
-
-    # Test negative inputs
-    assert np.isnan(pythagoras(-3, 4))
-    assert np.isnan(pythagoras(3, -4))
-    assert np.isnan(pythagoras(-3, -4))
+        a = 3
+        b = 4
+        expected_result = 5
+        
+        result = pythagoras(a, b)
+        
+        self.assertEqual(result, expected_result)
